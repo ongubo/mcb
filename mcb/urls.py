2762e764c834835cf6c2969bea2c5613cbf6e5cb
ongubo <developerworks25@gmail.com>
2019-02-22 20:15:00 +0300

    Add urls for api

--- mcb/urls.py
+++ mcb/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -8,4 +8,7 @@ urlpatterns = [
         '',
         include('loan.urls')
     ),
+    path('api/', include('api.urls')),
+    # re_path('api/(?P<version>(v1|v2))/', include('api.urls'))
+
 ]
